86e94e4fc1c0d02c9044f0f9623fa3ea7281666e
michael <michael@cadot.info>
2021-01-15 18:58:21 +0100

    clean

--- formidriveapi.sh
+++
@@ -1,6 +0,0 @@
-docker run -d -p :80  \
---name formidriveapi \
---label traefik.frontend.rule=Host:formidriveapi.cadot.info,www.formidriveapi.cadot.info \
---label traefik.enable=true \
--v /home/docker/sites/formidriveapi:/var/www/html  \
-cadotinfo/opencv
